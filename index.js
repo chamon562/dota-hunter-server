484b23a7ddb0cdb45e0dca4b32103556a05524cc
Chamon562 <Chamon562@gmail.com>
2020-09-01 19:52:32 -0700

    [index] Add app.use json and urlencoded and one get route

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const express = require('express')
 const app = express()
 
+
+// for the form data to be processed into req.body
 app.use(express.urlencoded({extended: false}))
+
+// express to recongise the req.body as a json object
+app.use(express.json())
+
+// including the controller
+app.use('/heroes', require('./routes/heroes'))
+
+// get route
+app.get('/', (req, res)=>{
+    res.send('This the Home Route to Dota Hunter server🦧')
+})
